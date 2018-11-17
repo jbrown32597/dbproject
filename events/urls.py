be15aa047bd2819a73aba3ee729701f4fe7777cf
Jay Brown <jbrown32597@gmail.com>
2018-11-17 15:46:19 -0500

    Prepared base navbar; installed Crispy Forms

--- events/urls.py
+++ events/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from .views import WelcomeView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'events'
 urlpatterns = [
     path('', WelcomeView.as_view(), name='welcome'),
-    path('login/', LoginView.as_view())
+    path('login/', LoginView.as_view(template_name='events/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='events/logout.html'), name='logout'),
 ]
